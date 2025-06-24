0af40843d178f90ae1086859cd5428900c2838b2
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-24 21:39:39 +0200

    Create a new task module to handle task operations

---
+++ src/modules/task.js
@@ -0,0 +1,27 @@
+export default class Task {
+  constructor(title, description, dueDate, priority, group) {
+    this._id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.group = group;
+    this.completed = false;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  toggleCompletion() {
+    this.completed = !this.completed;
+  }
+
+  updateDetails({ title, description, dueDate, priority, group }) {
+    if (title) this.title = title;
+    if (description) this.description = description;
+    if (dueDate) this.dueDate = dueDate;
+    if (priority) this.priority = priority;
+    if (group) this.group = group;
+  }
+}
