3999c0a75abe444124321ef33b6ee32562ac3e20
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-24 21:40:24 +0200

    Create task list module for managing tasks
    This module provides functionality to create, update, and delete tasks.

---
+++ src/modules/task-list.js
@@ -0,0 +1,23 @@
+import Task from "./task.js";
+
+export default class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  addTask(task) {
+    if (task instanceof Task) {
+      this.tasks.push(task);
+    } else {
+      throw new Error("Invalid task");
+    }
+  }
+
+  removeTask(taskId) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  editTask(taskId, newTaskDetails) {
+    this.tasks.find((task) => task.id === taskId).updateDetails(newTaskDetails);
+  }
+}
