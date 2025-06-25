3b503e1fe99ee6c3b35b8a7fa3e6ed7e901db3dd
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-25 21:22:00 +0200

    Test tasks functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
+import TaskList from "./modules/task-list";
+import Task from "./modules/task";
+import displayTasks from "./modules/display-tasks";
+import { getTaskList } from "./modules/storage";
 
-console.log("Hi!");
+const list = getTaskList();
+list.addTask(
+  new Task("Title1", "Description", "20-07-2025", "high", "Homework")
+);
+list.addTask(new Task("Title2", "", "20-08-2025", "low", "Job"));
+list.addTask(
+  new Task(
+    "Title aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsum iusto officia similique nobis. Ipsam fugiat neque laborum totam voluptatum unde beatae temporibus, dolor alias. Corrupti, repellat? Totam, ipsa sunt!",
+    "10-07-2025",
+    "medium",
+    "Homework"
+  )
+);
+
+displayTasks(list.tasks);
