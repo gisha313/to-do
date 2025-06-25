b27ec18a2e7b402fece47a9cc95d7d147e896a7b
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-25 19:26:13 +0200

    Create an event handler module to manage events
    Currently contains the logic for handling task status changes and task
    details display.

---
+++ src/modules/event-handler.js
@@ -0,0 +1,46 @@
+import { getTaskList } from "./storage";
+
+function expandTaskContent(taskDiv) {
+  const taskContent = taskDiv.querySelector(".task-content");
+  const expandBtn = taskDiv.querySelector(".expand-task-btn");
+
+  if (expandBtn.dataset.expanded !== "true") {
+    expandBtn.dataset.expanded = "true";
+    taskContent.classList.remove("hidden");
+  }
+}
+
+function collapseTaskContent(taskDiv) {
+  const taskContent = taskDiv.querySelector(".task-content");
+  const expandBtn = taskDiv.querySelector(".expand-task-btn");
+  if (!taskContent) return;
+
+  if (expandBtn.dataset.expanded === "true") {
+    expandBtn.dataset.expanded = "false";
+    taskContent.classList.add("hidden");
+  }
+}
+
+export function toggleTaskContent(event) {
+  const expandTaskBtn = event.target;
+  const taskDiv = expandTaskBtn.parentElement.parentElement.parentElement;
+  const expandBtn = taskDiv.querySelector(".expand-task-btn");
+
+  if (expandBtn.dataset.expanded === "true") collapseTaskContent(taskDiv);
+  else expandTaskContent(taskDiv);
+}
+
+export function changeTaskStatus(event) {
+  const taskStatusButton = event.target;
+  const taskDiv = taskStatusButton.parentElement.parentElement;
+  const taskId = taskDiv.dataset.id;
+  const taskList = getTaskList();
+  const task = taskList.getTask(taskId);
+
+  if (task.isCompleted) taskDiv.classList.remove("done");
+  else taskDiv.classList.add("done");
+
+  task.toggleCompletion();
+
+  collapseTaskContent(taskDiv);
+}
