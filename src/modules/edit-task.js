d5dc2c1350d48fed029a9f6fe05fbb464624b84e
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-26 16:53:09 +0200

    Fix editing task with no prior description

--- src/modules/edit-task.js
+++ src/modules/edit-task.js
@@ -1,100 +1,110 @@
-import { getTaskById  } from "./storage";
+import { getTaskById } from "./storage";
 import { createTaskElement } from "./display-tasks";
 
 function getDataFromTaskDiv(taskDiv) {
-    const taskTitle = taskDiv.querySelector("h3").textContent;
-    const taskDescription = taskDiv.querySelector(".task-content p").textContent ? taskDiv.querySelector(".task-content p").textContent : "";
-    const taskDueDate = taskDiv.querySelector(".task-date").textContent.replace("Due: ", "");
+  const taskTitle = taskDiv.querySelector("h3").textContent;
+  const taskDescription = taskDiv.querySelector(".task-content p")
+    ? taskDiv.querySelector(".task-content p").textContent
+    : "";
+  const taskDueDate = taskDiv
+    .querySelector(".task-date")
+    .textContent.replace("Due: ", "");
 
-
-    return {
-        title: taskTitle,
-        description: taskDescription,
-        dueDate: taskDueDate,
-    };
+  return {
+    title: taskTitle,
+    description: taskDescription,
+    dueDate: taskDueDate,
+  };
 }
 
 function createEditForm(taskDiv) {
-    const taskData = getDataFromTaskDiv(taskDiv);
-    console.table(taskData);
+  const taskData = getDataFromTaskDiv(taskDiv);
+  console.table(taskData);
 
-    const form = document.createElement("form");
-    form.classList.add("edit-task-form");
+  const form = document.createElement("form");
+  form.classList.add("edit-task-form");
 
-    const header = document.createElement("div");
-    header.classList.add("task-header");
-    
-    const taskStatusButton = document.createElement("button");
-    taskStatusButton.classList.add("task-status");
-    taskStatusButton.disabled = true;
+  const header = document.createElement("div");
+  header.classList.add("task-header");
 
-    header.appendChild(taskStatusButton);
+  const taskStatusButton = document.createElement("button");
+  taskStatusButton.classList.add("task-status");
+  taskStatusButton.disabled = true;
 
-    const taskTitle = document.createElement("h3");
-    taskTitle.innerHTML = `<input type="text" name="edit-task-title" id="edit-task-title" value="${taskData.title}" maxlength="30" required spellcheck="false" autocomplete="off">`;
-    header.appendChild(taskTitle);
+  header.appendChild(taskStatusButton);
 
-    form.appendChild(header);
+  const taskTitle = document.createElement("h3");
+  taskTitle.innerHTML = `<input type="text" name="edit-task-title" id="edit-task-title" value="${taskData.title}" maxlength="30" required spellcheck="false" autocomplete="off">`;
+  header.appendChild(taskTitle);
 
-    const taskContent = document.createElement("div");
-    taskContent.classList.add("task-content");
+  form.appendChild(header);
 
-    const taskDescription = document.createElement("p");
-    taskDescription.innerHTML = `<textarea name="edit-task-description" id="edit-task-description" oninput="dynamicHeight(this)">${taskData.description}</textarea>`
-    taskContent.appendChild(taskDescription);
-    form.appendChild(taskContent);
+  const taskContent = document.createElement("div");
+  taskContent.classList.add("task-content");
 
-    const taskFooter = document.createElement("div");
-    taskFooter.classList.add("task-footer");
+  const taskDescription = document.createElement("p");
+  taskDescription.innerHTML = `<textarea name="edit-task-description" id="edit-task-description" oninput="dynamicHeight(this)">${taskData.description}</textarea>`;
+  taskContent.appendChild(taskDescription);
+  form.appendChild(taskContent);
 
-    const taskDueDate = document.createElement("span");
-    taskDueDate.classList.add("task-date");
-    //add date convertion
-    taskDueDate.innerHTML = `Due: <input type="date" name="edit-task-date" id="edit-task-date" value="${taskData.dueDate}">`
-    taskFooter.appendChild(taskDueDate);
+  const taskFooter = document.createElement("div");
+  taskFooter.classList.add("task-footer");
 
-    const editTaskActions = document.createElement("div");
-    const saveBtn = document.createElement("button");
-    saveBtn.classList.add("edit-save-btn");
-    saveBtn.textContent = "Save";
+  const taskDueDate = document.createElement("span");
+  taskDueDate.classList.add("task-date");
+  //add date convertion
+  taskDueDate.innerHTML = `Due: <input type="date" name="edit-task-date" id="edit-task-date" value="${taskData.dueDate}">`;
+  taskFooter.appendChild(taskDueDate);
 
-    saveBtn.addEventListener("click", saveNewTaskData)
+  const editTaskActions = document.createElement("div");
+  const saveBtn = document.createElement("button");
+  saveBtn.classList.add("edit-save-btn");
+  saveBtn.textContent = "Save";
 
-    editTaskActions.appendChild(saveBtn);
+  saveBtn.addEventListener("click", saveNewTaskData);
 
-    const cancelBtn = document.createElement("button");
-    cancelBtn.classList.add("edit-cancel-btn");
-    cancelBtn.textContent = "Cancel";
-    editTaskActions.appendChild(cancelBtn);
+  editTaskActions.appendChild(saveBtn);
 
-    taskFooter.appendChild(editTaskActions);
-    form.appendChild(taskFooter);
+  const cancelBtn = document.createElement("button");
+  cancelBtn.classList.add("edit-cancel-btn");
+  cancelBtn.textContent = "Cancel";
+  editTaskActions.appendChild(cancelBtn);
 
-    return form;
+  taskFooter.appendChild(editTaskActions);
+  form.appendChild(taskFooter);
+
+  return form;
 }
 
 function saveNewTaskData(event) {
-    event.preventDefault();
-    const taskDiv = event.target.parentElement.parentElement.parentElement.parentElement;
-    const newTitle = taskDiv.querySelector("#edit-task-title").value;
-    const newDescription = taskDiv.querySelector("#edit-task-description").value ?  taskDiv.querySelector("#edit-task-description").value : "";
-    const newDueDate = taskDiv.querySelector("#edit-task-date").value;
+  event.preventDefault();
+  const taskDiv =
+    event.target.parentElement.parentElement.parentElement.parentElement;
+  const newTitle = taskDiv.querySelector("#edit-task-title").value;
+  const newDescription = taskDiv.querySelector("#edit-task-description").value
+    ? taskDiv.querySelector("#edit-task-description").value
+    : "";
+  const newDueDate = taskDiv.querySelector("#edit-task-date").value;
 
-    const newData = {title: newTitle, description: newDescription, dueDate: newDueDate}
+  const newData = {
+    title: newTitle,
+    description: newDescription,
+    dueDate: newDueDate,
+  };
 
-    const taskId = taskDiv.dataset.id;
-    const task = getTaskById(taskId);
-    task.updateDetails(newData);
+  const taskId = taskDiv.dataset.id;
+  const task = getTaskById(taskId);
+  task.updateDetails(newData);
 
-    const newTaskDiv = createTaskElement(task);
-    const wrapper = taskDiv.parentElement;
-    wrapper.innerHTML = "";
-    wrapper.appendChild(newTaskDiv);
+  const newTaskDiv = createTaskElement(task);
+  const wrapper = taskDiv.parentElement;
+  wrapper.innerHTML = "";
+  wrapper.appendChild(newTaskDiv);
 }
 
 export function openEditTaskForm(taskDiv) {
-    const form = createEditForm(taskDiv);
-    taskDiv.innerHTML = "";
+  const form = createEditForm(taskDiv);
+  taskDiv.innerHTML = "";
 
-    taskDiv.appendChild(form);
+  taskDiv.appendChild(form);
 }
