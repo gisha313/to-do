3db5617859941cd9069c248c1153320c7fb4d028
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-27 20:30:37 +0200

    Keep group names in storage, add funcs for them
    Also change the way addTask function works

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,6 +1,8 @@
 import TaskList from "./task-list";
+import Task from "./task";
 
 const taskList = new TaskList();
+const groupList = ["Homework", "Job", "Personal"];
 
 export function getTaskList() {
   return taskList;
@@ -10,6 +12,18 @@ export function getTaskById(taskId) {
   return taskList.getTask(taskId);
 }
 
+export function getGroupList() {
+  return groupList;
+}
+
+export function addGroup(group) {
+  groupList.add(group);
+}
+
 export function removeTaskById(taskId) {
   taskList.removeTask(taskId);
+}
+
+export function addTask(taskData) {
+  taskList.addTask(new Task(...taskData));
 }
