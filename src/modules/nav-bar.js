af6e73d54cfc90408e6d7af9b168cb328588ce43
gisha <52324253+gisha313@users.noreply.github.com>
2025-06-27 20:29:46 +0200

    Add function that displays groups from 'storage'

--- src/modules/nav-bar.js
+++ src/modules/nav-bar.js
@@ -1,7 +1,26 @@
 import { openAddTaskForm } from "./event-handler";
+import { getGroupList } from "./storage";
+
+function displayGroupElements() {
+  const ulGroups = document.querySelector(".group-list");
+  const groups = getGroupList();
+  groups.forEach((groupName) => {
+    const li = document.createElement("li");
+    const navItem = document.createElement("div");
+    navItem.classList.add("nav-item");
+
+    const a = document.createElement("a");
+    a.textContent = groupName;
+
+    navItem.appendChild(a);
+    li.appendChild(navItem);
+    ulGroups.appendChild(li);
+  });
+}
 
 export default function initializeNavBar() {
   const nav = document.querySelector("nav");
   const addTaskBtn = nav.querySelector("#add-task-btn");
   addTaskBtn.addEventListener("click", openAddTaskForm);
+  displayGroupElements();
 }
